fd37580ff707da8b00a0ea306cdc3c5703b79838
Jeff Ramnani <jramnani@8thlight.com>
2016-01-26 14:54:25 -0600

    Add URL to the repo in the project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject game-of-life "0.1.0-SNAPSHOT"
   :description "Conway's Game of Life"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jramnani/game-of-life"
   :license {:name "MIT License"
             :url "http://choosealicense.com/licenses/mit/"}
   :dependencies [[org.clojure/clojure "1.7.0"]
