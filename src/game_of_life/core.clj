5051bfe16ef40999360ef7149c022eb83ded8f92
Jeff Ramnani <jramnani@8thlight.com>
2016-01-26 16:45:01 -0600

    Can get the neighbors for a given cell at x and y.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,15 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
+
+(defn neighbors
+  "([[x y]])
+  Takes a two-item vector of integers representing the cell at coordinates x and y.
+  Returns a list of coordinates of the cell's eight neighbors."
+  [[x y]]
+  (for [dx [-1 0 1]  ;; left, center, and right.
+        ;; dx = 0, dy = 0 is the same as x,y.
+        ;; Don't let a cell be a neighbor to itself.
+        dy (if (zero? dx)
+             [-1 1]
+             [-1 0 1])]
+    [(+ dx x) (+ dy y)]))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-basic-test
-  (testing "FIXME: I'm a failing test."
-    (is (= true false))))
+;; Given that a 'cell' is a two item vector of integers containing x,y coordinates.
+;; e.g. [1 2]
+
+;; And the 'world' is a set of two item vectors containing x,y
+;; coordinates.
+;; e.g. #{[0 0] [0 1] [0 2] [0 1] [1 1] [1 2]}
+
+;; And only 'live' cells are contained in the set.  Dead cells are omitted.
+;; For example, a 'blinker' automata would oscillate between these two states.
+;; [0 0] [1 0] [2 0] -> [1 0] [1 1] [1 2]
+
+(deftest neighbors-test
+  (testing "neighbors should find the eight neighbors for a given cell."
+    (is (= (sort '([0 0] [1 0] [2 0] [0 1] [2 1] [0 2] [1 2] [2 2]))
+           (sort (neighbors [1 1]))))))
