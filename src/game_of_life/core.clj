787c2998a2337bc90fce55d65027c25c05889f04
Jeff Ramnani <jramnani@8thlight.com>
2016-01-27 12:04:30 -0600

    Factor out a 'world' data structure.
    In order to implement a "wrapped" world, we need to know the dimensions
    of the world.  Using a HashMap to keep the height, width, and living
    cells together in one place.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,9 +1,19 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
 
+(defn create-world
+  "Create a HashMap with the keys:
+  height: integer
+  width: integer
+  cells: set of [x,y] coordinate pairs of live cells. e.g. #{[0 0] [0 1] [0 2] [1 2]}"
+  ([height width]
+   {:height height :width width :cells #{}})
+  ([height width cells]
+   {:height height :width width :cells (set cells)}))
+
+
 (defn neighbors
-  "([[x y]])
-  Takes a two-item vector of integers representing the cell at coordinates x and y.
+  "Takes a two-item vector of integers representing the cell at coordinates x and y.
   Returns a list of coordinates of the cell's eight neighbors."
   [[x y]]
   (for [dx [-1 0 1]  ;; left, center, and right.
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -13,6 +13,16 @@
 ;; For example, a 'blinker' automata would oscillate between these two states.
 ;; [0 0] [1 0] [2 0] -> [1 0] [1 1] [1 2]
 
+(deftest create-world-test
+  (testing "Creating a world with only height and width will return no living cells."
+    (let [world (create-world 10 10)]
+      (is (= #{} (:cells world)))))
+
+  (testing "Can create a world with a set of living cells."
+    (let [world (create-world 10 10 #{[1 1]})]
+      (is (contains? (:cells world) [1 1])))))
+
+
 (deftest neighbors-test
   (testing "neighbors should find the eight neighbors for a given cell."
     (is (= (sort '([0 0] [1 0] [2 0] [0 1] [2 1] [0 2] [1 2] [2 2]))
@@ -23,3 +33,14 @@
           y 1
           the-neighbors (neighbors [x y])]
       (is (not (contains? (set the-neighbors) [x y]))))))
+
+;; (deftest wrapping-world-test
+;;   (testing "A cell on the right edge of the world has a neighbor on the left edge."
+;;     ;; Given a 3x3 world.
+;;     (let [world #{[3 1] [0 1]}
+;;           right-x 3
+;;           right-y 1
+;;           left-x 0
+;;           left-y 1
+;;           the-neighbors (neighbors [right-x right-y])]
+;;       (is (contains? (set the-neighbors) [left-x left-y])))))
