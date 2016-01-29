1c84247d0dfec349a7b2f18978a3547f66807119
Jeff Ramnani <jramnani@8thlight.com>
2016-01-29 10:02:49 -0600

    Can create a world and get naive list of neighbors.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,16 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
+
+
+(defn create-world
+  [height width cells]
+  {:height height :width width :cells cells})
+
+
+(defn neighbors
+  [x y]
+  (for [dx [-1 0 1]
+        dy (if (zero? dx)
+             [-1 1]
+             [-1 0 1])]
+    [(+ dx x) (+ dy y)]))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-basic-test
-  (testing "FIXME: I'm a failing test."
-    (is (= true false))))
+(deftest world-test
+  (testing "A world is a map of dimensions and live cells"
+    (let [expected-world {:height 10 :width 10 :cells #{[0 1]}}]
+      (is (= expected-world (create-world 10 10 #{[0 1]}))))))
+
+(deftest neighbors-test
+  (testing "A cell at x,y has eight neighbors."
+    (let [x 1 y 1]
+      (is (= 8 (count (neighbors x y)))))))
