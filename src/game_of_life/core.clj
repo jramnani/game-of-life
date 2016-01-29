ead492a0cf1656d7a966d9d39e9b93a59f598230
Jeff Ramnani <jramnani@8thlight.com>
2016-01-29 14:06:56 -0600

    Add main method to run the game.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -50,3 +50,13 @@
                         :when (live n (contains? cells cell))]
                     cell)]
     (assoc world :cells (set new-cells))))
+
+(defn -main
+  []
+  (def height 20)
+  (def width 80)
+  (def total-cells (* height width))
+  (def random-cells (set (repeatedly (/ total-cells 4)
+                                     #(vec [(rand-int width) (rand-int height)]))))
+  (def world (create-world height width random-cells))
+  (display-world-to-terminal world step))
