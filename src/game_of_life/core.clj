5da04d96adff5154d9cd41467a4e2083d60369fc
Jeff Ramnani <jramnani@8thlight.com>
2016-01-29 10:46:15 -0600

    Compute world wrapping for the list of neighbors.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -14,3 +14,23 @@
              [-1 1]
              [-1 0 1])]
     [(+ dx x) (+ dy y)]))
+
+(defn world-wrap-neighbors
+  [neighbors world]
+  (let [max-height (dec (:height world))
+        max-width (dec (:width world))
+        xs (map first neighbors)
+        ys (map second neighbors)
+        wrapped-xs (map (fn [x]
+                          (cond
+                            (< 0 x) max-width
+                            (> x max-width) 0
+                            :else x))
+                        xs)
+        wrapped-ys (map (fn [y]
+                          (cond
+                            (< 0 y) max-height
+                            (> y max-height) 0
+                            :else y))
+                        ys)]
+    (map #(vec [%1 %2]) wrapped-xs wrapped-ys)))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -9,5 +9,19 @@
 
 (deftest neighbors-test
   (testing "A cell at x,y has eight neighbors."
-    (let [x 1 y 1]
+    (let [x 1 y 1
+          world (create-world 3 3 #{})]
       (is (= 8 (count (neighbors x y)))))))
+
+(deftest wrapped-world-test
+  (testing "Cells on opposite sides of the board are adjacent."
+    (let [world (create-world 3 3 #{})
+          x 0 y 0
+          the-neighbors (world-wrap-neighbors (neighbors x y) world)
+          ;; expected neighbors
+          top [0,2]
+          left [2 0]
+          top-left [2,2]]
+      (is (contains? (set the-neighbors) top))
+      (is (contains? (set the-neighbors) left))
+      (is (contains? (set the-neighbors) top-left)))))
