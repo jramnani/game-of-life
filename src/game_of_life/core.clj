6a0d08e3ff907d102266bfd725686dc7566274ad
Jeff Ramnani <jramnani@8thlight.com>
2016-02-03 17:17:21 -0600

    Finished kata for the day.
    Had to brush off some rust.  Haven't done this in too long.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,45 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
+
+(defn create-world [height width cells]
+  {:height height :width width :cells cells})
+
+(defn neighbors [[x y]]
+  (for [dx [-1 0 1]
+        dy (if (zero? dx)
+             [-1 1]
+             [-1 0 1])]
+    [(+ dx x) (+ dy y)]))
+
+(defn world-wrap-neighbors [neighbors world]
+  (let [max-height (dec (:height world))
+        max-width (dec (:width world))
+        xs (map first neighbors)
+        ys (map second neighbors)
+        wrapped-xs (map (fn [x]
+                          (cond
+                            (< x 0) max-width
+                            (> x max-width) 0
+                            :else x))
+                        xs)
+        wrapped-ys (map (fn [y]
+                          (cond
+                            (< y 0) max-height
+                            (> y max-height) 0
+                            :else y))
+                        ys)]
+    (map #(vec [%1 %2]) wrapped-xs wrapped-ys)))
+
+(defn live [n alive?]
+  (or (= n 3)
+      (and (= n 2)
+           alive?)))
+
+(defn step [world]
+  (let [cells (:cells world)
+        the-neighbors (mapcat neighbors cells)
+        wrapped-neighbors (world-wrap-neighbors the-neighbors world)
+        new-cells (for [[cell n] (frequencies wrapped-neighbors)
+                        :when (live n (contains? cells cell))]
+                    cell)]
+    (assoc world :cells (set new-cells))))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,52 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-basic-test
-  (testing "FIXME: I'm a failing test."
-    (is (= true false))))
+
+(deftest world-test
+  (testing "A world is a map of height, width, and live cells."
+    (let [expected-world {:height 10 :width 10 :cells #{[0,1]}}]
+      (is (= expected-world (create-world 10 10 #{[0,1]}))))))
+
+(deftest neighbors-test
+  (testing "A cell at x,y has eight neighbors."
+    (let [x 1 y 1]
+      (is (= 8 (count (neighbors [x y])))))))
+
+(deftest wrapped-world-test
+  (testing "Cells on opposite sites of the board are adjacent."
+    (let [world (create-world 3 3 #{})
+          x 0 y 0
+          the-neighbors (world-wrap-neighbors (neighbors [x y]) world)
+          bottom [0,2]
+          left [2,0]
+          bottom-right [2,2]]
+      (is (contains? (set the-neighbors) bottom))
+      (is (contains? (set the-neighbors) left))
+      (is (contains? (set the-neighbors) bottom-right)))))
+
+(deftest live-test
+  (let [alive true
+        dead false]
+  (testing "A dead cell with two neighbors is still dead."
+    (is (= dead (live 2 dead))))
+  
+  (testing "A dead cell with three neighbors comes to life."
+    (is (= alive (live 3 dead))))
+    
+  (testing "A cell with two or three neighbors stays alive."
+    (is (= alive (live 3 alive))))
+    (is (= alive (live 2 alive)))
+    
+  (testing "A cell with more than three neighbors dies of overpopulation."
+    (is (= dead (live 4 alive))))
+    
+  (testing "A cell will fewer than two neighbors dies of underpopulation."
+    (is (= dead (live 1 alive))))))
+
+(deftest blinker-test
+  (testing "A 'blinker' oscillates between a horizontal and vertical line."
+    (let [horizontal-blinker #{[1,1] [2,1] [3,1]}
+          vertical-blinker #{[2,0] [2,1] [2,2]}
+          initial-world (create-world 5 5 horizontal-blinker)]
+      (is (= vertical-blinker (:cells (step initial-world))))
+      (is (= horizontal-blinker (:cells (step (step initial-world))))))))
