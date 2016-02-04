6b7e010a65ad88f5a9e007d667c01cef18404121
Jeff Ramnani <jramnani@8thlight.com>
2016-02-04 10:58:18 -0600

    WIP: Fleshing out my idea for world wrapping.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,16 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
+
+(defn create-world [height width cells]
+  {:height height :width width :cells cells})
+
+(defn get-neighbors [[x y]]
+  (for [dx [-1 0 1]
+        dy (if ( zero? dx)
+             [-1 1]
+             [-1 0 1])]
+    [(+ dx x) (+ dy y)]))
+
+(defn world-wrap-val [value max-value]
+  (cond
+    (> 0 value) max-value))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,6 +2,22 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-basic-test
-  (testing "FIXME: I'm a failing test."
-    (is (= true false))))
+(deftest world-test
+  (testing "A world is a map of dimensions and live cells."
+    (let [expected-world {:height 5 :width 10 :cells #{}}]
+      (is (= expected-world (create-world 5 10 #{}))))))
+
+
+(deftest get-neighbors-test
+  (testing "A cell has 8 neighbors."
+    (let [x 1 y 1
+          neighbors (get-neighbors [x y])]
+      (is (= 8 (count neighbors))))))
+
+(deftest world-wrap-val-test
+  (testing "Given a max width of 3 a coordinate at -1 will wrap into 2."
+    (let [x -1
+          max-width 2
+          expected 2]
+      (is (= expected (world-wrap-val x max-width))))))
+
