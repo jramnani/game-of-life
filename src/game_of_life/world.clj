61097c4b09a45166f1c818f8762759eed959e41c
Jeff Ramnani <jramnani@8thlight.com>
2016-02-05 11:06:05 -0600

    Document functions for the 'world' module.

--- src/game_of_life/world.clj
+++ src/game_of_life/world.clj
@@ -1,10 +1,15 @@
 (ns game-of-life.world)
 
 (defn create-world [height width cells]
+  "A world is a map with keys, :height, :width, and :cells. The height and
+  width are the dimensions of the world grid, and the cells are a set of [x,y]
+  coordinate pairs that represent living cells."
   {:height height :width width :cells cells})
 
 
 (defn get-neighbors [[x y]]
+  "Given an [x,y] coordintate of a cell, return it's eight neighbors on the
+  world grid as a list of coordinate pairs."
   (for [dx [-1 0 1]
         dy (if (zero? dx)
              [-1 1]
@@ -13,6 +18,8 @@
 
 
 (defn world-wrap-val [value max-value]
+  "Given the int's value and max-value, clamp the value to be within:
+  0 <= value <= max-value."
   (cond
     (< value 0) max-value
     (> value max-value) 0
@@ -20,6 +27,10 @@
 
 
 (defn world-wrap-neighbors [neighbors world]
+  "Given a list of neighbors and a world map, transform the neighbors to be
+  within the dimensions of the world's grid. This makes cells on the opposite
+  of the grid be adjacent.
+  Implements a 'wrapped world' in Conway's Game of Life terminology."
   (let [max-height (dec (:height world))
         max-width (dec (:width world))
         xs (map first neighbors)
