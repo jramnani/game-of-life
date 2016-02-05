c989b72e4e21f4f9fc0aff7d7089581f98de5522
Jeff Ramnani <jramnani@8thlight.com>
2016-02-05 10:52:34 -0600

    Implement a wrapped world.
    Take a list of neighbors and transform them so that cell's that were out
    of range are within the boundaries of the world's grid.

--- test/game_of_life/world_test.clj
+++ test/game_of_life/world_test.clj
@@ -41,3 +41,19 @@
     (let [just-right 1
           max-value 2]
       (is (= just-right (world-wrap-val just-right max-value))))))
+
+
+(deftest world-wrap-neighbors-test
+  (testing "Given a 3x3 world: cells on opposite sites of the world are adjacent."
+    (let [top-left-cell [0,0]
+          world (create-world 3 3 #{})
+          neighbors (get-neighbors top-left-cell)
+          wrapped-neighbors (world-wrap-neighbors neighbors world)
+          ;; Expected neighbors named after their place on the world grid.
+          top-right [2,0]
+          top-middle [1,0]
+          bottom-left [0,2]
+          bottom-right [2,2]]
+
+      (is (some #{top-right top-middle bottom-left bottom-right}
+                wrapped-neighbors)))))
