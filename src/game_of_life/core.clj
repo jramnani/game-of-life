0540ef0c27c7224865347de799ef0db1622249f0
Jeff Ramnani <jramnani@8thlight.com>
2016-02-08 13:56:32 -0600

    Finished rules checking.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,7 @@
 (ns game-of-life.core
   (:require [game-of-life.terminal-display :refer :all]))
+
+(defn live [n alive?]
+  (or (= n 3)
+      (and (= n 2)
+           alive?)))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -2,3 +2,22 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
+
+(deftest live-test
+  (let [alive true
+        dead false]
+  (testing "A dead cell with two neighbors stays dead."
+    (is (= dead (live 2 dead))))
+
+  (testing "A dead cell with three neighbors becomes alive."
+    (is (= alive (live 3 dead))))
+
+  (testing "A live cell with two or three neighbors stays alive."
+    (is (= alive (live 2 alive)))
+    (is (= alive (live 3 alive))))
+
+  (testing "A live cell with more than three neighbors dies of overpopulation."
+    (is (= dead (live 4 alive))))
+
+  (testing "A live cell with less than two neighbors dies of underpopulation."
+    (is (= dead (live 1 alive))))))
