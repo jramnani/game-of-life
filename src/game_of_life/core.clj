78103fe031bfbff346816548bf8b892ae3deeb59
Jeff Ramnani <jramnani@8thlight.com>
2016-02-09 11:57:30 -0600

    Add boilerplate for a static world by default.
    This boilerplate reduces the scope of the kata to a more manageable
    level.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,2 +1,19 @@
 (ns game-of-life.core
-  (:require [game-of-life.terminal-display :refer :all]))
+  (:require [game-of-life.terminal-display :refer :all]
+            [game-of-life.world :refer :all]))
+
+
+(defn step [world]
+  world)
+
+
+(defn -main []
+  (def height 20)
+  (def width 80)
+  (def total-cells (* height width))
+  (def random-cells (set (repeatedly (/ total-cells 4)
+                                #(vec [(rand-int width) (rand-int height)]))))
+
+  (def world (create-world height width random-cells))
+
+  (display-world-to-terminal world step))
