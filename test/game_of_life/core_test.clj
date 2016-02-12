8de2c552536cbf6427f9654add8288b62be1dac7
Jeff Ramnani <jramnani@8thlight.com>
2016-02-12 10:16:12 -0600

    Add placeholder for the final integration test.
    This test confirms that the solution works.

--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -3,3 +3,12 @@
             [game-of-life.world :refer :all]
             [game-of-life.core :refer :all]))
 
+
+;(deftest step-test
+  ;(testing "A blinker oscillates between a horizontal and vertical line."
+    ;(let [horizontal-blinker #{[0,1] [1,1] [2,1]}
+          ;vertical-blinker #{[1,0] [1,1] [1,2]}
+          ;world (create-world 5 5 horizontal-blinker)]
+      ;(is (= vertical-blinker (:cells (step world))))
+      ;(is (= horizontal-blinker (:cells (step (step world))))))))
+
