97429e63969b7b3bcf018539377cabbcea431fd5
Jeff Ramnani <jramnani@8thlight.com>
2016-02-18 10:21:23 -0600

    Finished kata for the day.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,6 +1,20 @@
 (ns game-of-life.core
   (:require [game-of-life.world :refer :all]))
 
+(defn live [n alive?]
+  (or (= n 3)
+      (and (= n 2)
+           alive?)))
+
+(defn next-cells [cells neighbors]
+  (let [neighbors-per-cell (frequencies neighbors)
+        new-cells (keys (filter (fn [[cell n]] (live n (contains? cells cell)))
+                                neighbors-per-cell))]
+    (set new-cells)))
 
 (defn step [world]
-  world)
+  (let [cells (:cells world)
+        neighbors (world-wrap-neighbors (mapcat get-neighbors cells)
+                                        world)
+        new-cells (next-cells cells neighbors)]
+    (assoc world :cells new-cells)))
