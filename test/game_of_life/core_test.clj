9904d194c1c324ec0c184d4aea71e00623d33d95
Jeff Ramnani <jramnani@8thlight.com>
2016-03-10 09:51:25 -0600

    finish kata for today.

--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -4,11 +4,49 @@
             [game-of-life.core :refer :all]))
 
 
-;(deftest step-test
-  ;(testing "A blinker oscillates between a horizontal and vertical line."
-    ;(let [horizontal-blinker #{[0,1] [1,1] [2,1]}
-          ;vertical-blinker #{[1,0] [1,1] [1,2]}
-          ;world (create-world 5 5 horizontal-blinker)]
-      ;(is (= vertical-blinker (:cells (step world))))
-      ;(is (= horizontal-blinker (:cells (step (step world))))))))
+(deftest next-cells-test
+  (testing "A world with no cells returns the empty set."
+    (is (= #{} (next-cells #{} []))))
+
+  (testing "A world with one cell returns the empty set."
+    (let [cells #{[1,1]}
+          neighbors (mapcat get-neighbors cells)]
+      (is (= #{} (next-cells cells neighbors)))))
+
+  (testing "A cell with three neighbors becomes alive."
+    (let [cells #{[0,1] [1,1] [2,1]}
+          neighbors (mapcat get-neighbors cells)
+          expected-cell [1,0]]
+      (is (contains? (next-cells cells neighbors)
+                     expected-cell)))))
+
+(deftest live-test
+  (let [alive true
+        dead false]
+    (testing "A cell with three neighbors is alive"
+      (is (= alive (live 3 alive)))
+      (is (= alive (live 3 dead))))
+
+    (testing "A live cell with two neighbors is alive."
+      (is (= alive (live 2 alive))))
+
+    (testing "A dead cell with two neighbors stays dead."
+      (is (= dead (live 2 dead))))
+
+    (testing "A cell with more than three neighbors dies of overpopulation."
+      (is (= dead (live 4 alive)))
+      (is (= dead (live 4 dead))))
+
+    (testing "A cell with less than two neighbors dies of underpopulation."
+      (is (= dead (live 1 alive)))
+      (is (= dead (live 1 dead))))))
+
+
+(deftest step-test
+  (testing "A blinker oscillates between a horizontal and vertical line."
+    (let [horizontal-blinker #{[0,1] [1,1] [2,1]}
+          vertical-blinker #{[1,0] [1,1] [1,2]}
+          world (create-world 5 5 horizontal-blinker)]
+      (is (= vertical-blinker (:cells (step world))))
+      (is (= horizontal-blinker (:cells (step (step world))))))))
 
